6677ebab3cb81791a055a438d9a0b09bd329493c
PRIYANSHU ANAND <122816376+iamprish121@users.noreply.github.com>
2024-07-02 20:10:23 +0530

    component updates

---
+++ src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DescriptionBox
--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -23,7 +23,7 @@ const ProductDisplay = (props) => {
 
       <div className="productdisplay-right">
         <h1> {product.name}</h1>
-        <div className="productdisplay-right-star">
+        <div className="productdisplay-right-stars">
             <img src={star_icon} alt="" />
             <img src={star_icon} alt="" />
             <img src={star_icon} alt="" />
@@ -31,7 +31,27 @@ const ProductDisplay = (props) => {
             <img src={star_dull_icon} alt="" />
             <p>(121)</p>
         </div>
-
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">${product.new_price}</div>
+        </div>
+        <div className="productdisplay-right-description">
+          resonable and easy to use .
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'> <span>Category: </span>Women, T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'> <span>Tags: </span>Modern, Latest</p>
       </div>
 
     </div>
